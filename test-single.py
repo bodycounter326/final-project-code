f4c791ef2655261c85cbbac2f9e96c9dcf4551cf
Emma Wang <seekerofmail@gmail.com>
2025-04-16 23:57:14 -0400

    both sensors should work now

--- test-single.py
+++ test-single.py
@@ -9,15 +9,18 @@ print("Program has started!")
 
 # Set XSHUT to BCM 16
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(16, GPIO.OUT)
+# GPIO.setup(16, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
 
 # Setup I2C early
 i2c = busio.I2C(board.SCL, board.SDA)
 
 # XSHUT sequence
-GPIO.output(16, GPIO.LOW)
+# GPIO.output(16, GPIO.LOW)
+GPIO.output(20, GPIO.LOW)
 time.sleep(0.1)
-GPIO.output(16, GPIO.HIGH)
+# GPIO.output(16, GPIO.HIGH)
+GPIO.output(20, GPIO.HIGH)
 print("XSHUT reset complete")
 time.sleep(0.5)
 
@@ -33,5 +36,5 @@ vl53 = VL53L0X(i2c)
 # Continuous measurement loop
 print("Starting distance measurement...")
 while True:
-    print("Distance: {} mm".format(vl53.range))
+    print("Distance: {} m".format((vl53.range) / 1000))
     time.sleep(1)
--- test-two-sensors.py
+++ test-two-sensors.py
@@ -4,8 +4,15 @@ from digitalio import DigitalInOut
 from adafruit_vl53l0x import VL53L0X
 
 # declare the singleton variable for the default I2C bus
-i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+
+import busio
+i2c = busio.I2C(board.SCL, board.SDA)
+while not i2c.try_lock():
+    pass
+i2c.unlock()
+
 
 # declare the digital output pins connected to the "SHDN" pin on each VL53L0X sensor
 xshut = [
@@ -22,22 +29,36 @@ vl53 = []
 
 # now change the addresses of the VL53L0X sensors
 for i, power_pin in enumerate(xshut):
+    print(f"Turning on sensor {i}")
     # turn on the VL53L0X to allow hardware check
     power_pin.value = True
+    time.sleep(0.25)
     # instantiate the VL53L0X sensor on the I2C bus & insert it into the "vl53" list
-    vl53.insert(i, VL53L0X(i2c))  # also performs VL53L0X hardware check
-    # no need to change the address of the last VL53L0X sensor
-    if i < len(xshut) - 1:
-        # default address is 0x29. Change that to something else
-        vl53[i].set_address(i + 0x30)  # address assigned should NOT be already in use
+    try:
+        sensor = VL53L0X(i2c)
+        # vl53.insert(i, sensor)
+        # vl53.insert(i, VL53L0X(i2c))  # also performs VL53L0X hardware check
+    
+        # no need to change the address of the last VL53L0X sensor
+        if i < len(xshut) - 1:
+            # default address is 0x29. Change that to something else
+            new_addr = 0x30 + i
+            print(f"Changing address to {hex(new_addr)}")
+            sensor.set_address(new_addr)  # address assigned should NOT be already in use
+        vl53.append(sensor)
 
+    except Exception as e:
+        print(f"Error initializing sensor {i}: {e}")
 
 
 def detect_range(count=5):
     """take count=5 samples"""
     while count:
         for index, sensor in enumerate(vl53):
-            print("Sensor {} Range: {}mm".format(index + 1, sensor.range))
+            try:
+                print(f"Sensor {index + 1} Range: {(sensor.range) / 1000}m")
+            except Exception as e:
+                print(f"Error from sensor {index+1}: {e}")
         time.sleep(1.0)
         count -= 1
 
